0b58805bf6c71372ded557331106ce27283bb0fa
Guillaume LAMIC <guillaumelamic@gmail.com>
2024-03-27 17:29:31 +0100

    horloge

--- src/js/scenarios/Scenario1.js
+++ src/js/scenarios/Scenario1.js
@@ -9,36 +9,36 @@ export default class Scenario1 extends Scene{
         // debug
         this.params.speed = 1; // = params speed des arcs
         this.params.color = 'white' ; // = params speed des arcs
-        this.params.lineWidth = 3; // = params taille lignes
+        this.params.lineWidth = 3; // = params taille lignes du cercle
+        this.params.handWidth = 1.5; // = params taille des aiguilles
         if (this.debug.active) {
             this.debugFolder.add(this.params, 'speed', 0, 4, 0.25);
-            this.debugFolder.add(this.params, 'color');
-            this.debugFolder.add(this.params, 'lineWidth', 1, 10, 1);
+            this.debugFolder.add(this.params, 'lineWidth', 1, 5, 0.3);
+            this.debugFolder.add(this.params, 'handWidth', 1, 5, 0.3);
         }
 
         this.resize();
-        const nArcs = 10;
+        const nArcs = 1;
 
         this.arcs = [];
-        for (let i =0; i < nArcs; i++) {
+        for (let i= 0; i < nArcs; i++) {
             const x_ = this.width / 2;
             const y_ = this.height / 2;
             const radius_ = this.mainRadius + (i - nArcs / 2)* this.deltaRadius;
-            const angle0_ = i != 0 && i <nArcs - 1 ? deg2rad( randomRange(0,360)) : 0;
-            const angle1_ = i != 0 && i <nArcs - 1 ? deg2rad( randomRange(0,360)) : 2 * Math.PI;
+            const angle0_ = i != 0 && i < nArcs - 1 ? deg2rad( randomRange(0,360)) : 0;
+            const angle1_ = i != 0 && i < nArcs - 1 ? deg2rad( randomRange(0,360)) : 2 * Math.PI;
 
             const arc_ = new RotatingArc( x_, y_, radius_, angle0_, angle1_ );
             this.arcs.push(arc_);
         }
     }
 
-    drawGraduation() {
-        const nGraduations = 12;
-        for (let i =0; i < nGraduations; i++) {
+    drawHoursGraduation(nGraduations =  12, arcLenght = 1.5) {
+        for (let i = 0 ; i < nGraduations; i++) {
             const angle_ = deg2rad(360) * i / nGraduations;
             const x_ = this.width / 2 + (this.mainRadius - this.deltaRadius / 2) * Math.cos(angle_);
             const y_ = this.height / 2 + (this.mainRadius - this.deltaRadius / 2) * Math.sin(angle_);
-            const length_ = (this.arcs.length -1) * this.deltaRadius;
+            const length_ = (this.arcs.length + arcLenght ) * this.deltaRadius;
 
             this.context.save();
             this.context.beginPath();
@@ -56,16 +56,45 @@ export default class Scenario1 extends Scene{
             this.context.restore();
         }
     }
+    drawMinutesGraduation() {
+        this.drawHoursGraduation(60, 0.25);
+    }
 
+    // Fonction pour dessiner le centre du cadran
+    drawCenterPoint() {
+        this.context.beginPath();
+        this.context.arc(this.width / 2, this.height / 2, 4, 0, 2 * Math.PI, false);
+        this.context.fillStyle = this.params.color;
+        this.context.fill();
+    }
+
+    // Fonction pour dessiner les aiguilles
+    drawHand(width , length, position) {
+        const angle = (new Date().getHours() % 12 + new Date().getMinutes() / 60) * 2 * Math.PI / position - Math.PI / 2;
+
+        this.context.beginPath();
+        this.context.moveTo(this.width / 2, this.height / 2);
+        this.context.lineTo(this.width / 2 + this.mainRadius * length * Math.cos(angle), this.height / 2 + this.mainRadius * length * Math.sin(angle));
+        this.context.strokeStyle = this.params.color;
+        this.context.lineWidth = width;
+        this.context.stroke();
+        this.context.closePath();
+    }
     update() {
         if (!super.update()) return;
         this.clear();
-        this.drawGraduation();
+        this.drawHoursGraduation();
+        this.drawMinutesGraduation();
+        this.drawCenterPoint();
+        this.drawHand(this.params.handWidth, 0.50, 12); // Heures
+        this.drawHand(this.params.handWidth, 0.65, 60); // Minutes
+        this.drawHand(this.params.handWidth, 0.85, 5); // Secondes
 
         this.arcs.forEach(arc => {
             arc.update(this.globalContext.time.delta, this.params.speed);
             arc.draw(this.context, this.params.color, this.params.lineWidth);
         })
+
     }
     resize() {
         super.resize();
--- src/main.js
+++ src/main.js
@@ -2,5 +2,4 @@ import Scene from "./js/canvas2d/Scene"
 import Scenario1 from "./js/scenarios/Scenario1";
 
 const scene = new Scenario1()// set id if != "canvas-scene" (default)
-const scene2 = new Scenario1("canvas-scene-2") // example
 
